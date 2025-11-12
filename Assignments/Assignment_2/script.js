34de323367dd2dbe760a96f853509611a1fabfa1
julieEple <julie.alme@yahoo.com>
2025-11-12 22:41:50 +0100

    make array of cells for grid
    2D array Cells= [row][column-cell]

--- Assignments/Assignment_2/script.js
+++ Assignments/Assignment_2/script.js
@@ -2,13 +2,21 @@
 document.addEventListener('DOMContentLoaded', () => { //to make sure the grid does not return null 
     const grid = document.getElementById('grid');
     const GRID_SIZE = 12;
-  
-    for (let y = 0; y < GRID_SIZE; y++) {
-      for (let x = 0; x < GRID_SIZE; x++) {
-        const cell = document.createElement('div'); 
-        cell.classList.add('cell');                  
-        grid.appendChild(cell);                      
-      }
+
+    const CELLS = []; //2D array of rows with columns
+    // Access a cell by CELLS[row][col]
+
+    for (let r = 0; r < GRID_SIZE; r++) { //will go through every row and all the columns it contains
+        const row = []; //makes new row for every 
+        for (let c = 0; c < GRID_SIZE; c++) {
+            const cell = document.createElement('div'); //cell created
+            cell.classList.add('cell');  //cell assigned new class cell                 
+            grid.appendChild(cell); //cell added to grid whoch works like a container
+            row.push(cell); //column-cell is added to the row-array
+    }
+    CELLS.push(row); //row is added to 2D array of rows with column cells
     }
   });
   
+  
+
